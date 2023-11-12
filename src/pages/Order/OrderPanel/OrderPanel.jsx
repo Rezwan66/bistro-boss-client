a789e33695d1e52482972d50b94e9a3b5a1c3966
Rezwan66 <shaikhrezwan66@gmail.com>
2023-11-13 03:49:39 +0600

    implemented pagination to the order tabs using swiper js pagination

--- src/pages/Order/OrderPanel/OrderPanel.jsx
+++ src/pages/Order/OrderPanel/OrderPanel.jsx
@@ -1,12 +1,22 @@
 import ProductCard from '../../Shared/ProductCard/ProductCard';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper/modules';
+import { Pagination, Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import './OrderPanel.css';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 
 const OrderPanel = ({ items }) => {
+  const count = items.length;
+  const itemsPerPage = 6;
+  const numberOfPages = Math.ceil(count / itemsPerPage);
+  const pages = [...Array(numberOfPages).keys()];
+  console.log(count, pages);
+
   const pagination = {
+    // el: '.swiper-pagination',
     clickable: true,
+    // type: 'fraction',
     renderBullet: function (index, className) {
       return '<span class="' + className + '">' + (index + 1) + '</span>';
     },
@@ -15,16 +25,30 @@ const OrderPanel = ({ items }) => {
     <div>
       <Swiper
         pagination={pagination}
-        modules={[Pagination]}
-        className="mySwiper"
+        navigation={{
+          nextEl: '.swiper-button-next-custom',
+          prevEl: '.swiper-button-prev-custom',
+        }}
+        modules={[Pagination, Navigation]}
+        className="mySwiper mb-10"
       >
-        <SwiperSlide>
-          <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10 max-w-screen-xl mx-auto mt-12 mb-20 px-6 lg:px-0">
-            {items?.map(item => (
-              <ProductCard key={item._id} item={item}></ProductCard>
-            ))}
-          </div>
-        </SwiperSlide>
+        {pages?.map(page => (
+          <SwiperSlide key={page}>
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10 max-w-screen-xl mx-auto mt-12 mb-20 px-6 lg:px-0">
+              {items?.map(item => (
+                <ProductCard key={item._id} item={item}></ProductCard>
+              ))}
+            </div>
+          </SwiperSlide>
+        ))}
+        <div className="my-10 flex justify-center gap-10">
+          <button className="swiper-button-prev-custom btn btn-circle bg-[#BB8506] border border-black">
+            <FaArrowLeft></FaArrowLeft>
+          </button>
+          <button className="swiper-button-next-custom btn btn-circle bg-[#BB8506] border border-black">
+            <FaArrowRight></FaArrowRight>
+          </button>
+        </div>
       </Swiper>
     </div>
   );
