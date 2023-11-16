2b8a2d6c71f7006bce3e5d5fcdff1e9fc5685cb2
Rezwan66 <shaikhrezwan66@gmail.com>
2023-11-17 02:43:54 +0600

    created Admin Route to secure admin routes

---
+++ src/Routes/AdminRoute.jsx
@@ -0,0 +1,23 @@
+import { Navigate, useLocation } from 'react-router-dom';
+import useAdmin from '../hooks/useAdmin';
+import useAuth from '../hooks/useAuth';
+import loaderImg from '../assets/others/loader3.gif';
+
+const AdminRoute = children => {
+  const { user, loading } = useAuth();
+  const { isAdmin, isAdminLoading } = useAdmin();
+  const location = useLocation();
+
+  if (loading || isAdminLoading) {
+    return (
+      <div className="flex items-center justify-center mt-20">
+        <img src={loaderImg} alt="" />
+      </div>
+    );
+  }
+  if (!user && !isAdmin) {
+    return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+  }
+  return children;
+};
+export default AdminRoute;
