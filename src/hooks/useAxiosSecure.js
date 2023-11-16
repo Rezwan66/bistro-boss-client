9f2fcb7cbf961958359736f8c56dbffb4ba67a3d
Rezwan66 <shaikhrezwan66@gmail.com>
2023-11-16 21:06:11 +0600

    used axios interceptor to intercept token response and request call

--- src/hooks/useAxiosSecure.js
+++ src/hooks/useAxiosSecure.js
@@ -5,6 +5,26 @@ const axiosSecure = axios.create({
 })
 
 const useAxiosSecure = () => {
+    // request interceptor to add authorization header for every secure call to the api
+    axiosSecure.interceptors.request.use(function (config) {
+        const token = localStorage.getItem('access-token')
+        console.log('req stopped by interceptor', token);
+        config.headers.authorization = `Bearer ${token}`;
+        return config;
+    }, function (error) {
+        // do sth with req error
+        return Promise.reject(error);
+    })
+
+    // intercept 401 and 403 status responses
+    axiosSecure.interceptors.response.use(function (response) {
+        return response;
+    }, (error) => {
+        const status = error.response.status;
+        console.log('status error in the interceptor:', status);
+        return Promise.reject(error);
+    })
+
     return axiosSecure;
 }
 export default useAxiosSecure;
