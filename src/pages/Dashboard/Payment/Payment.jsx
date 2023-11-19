936a22de2edf6335d28e024f3d3bda8c0373e95c
Rezwan66 <shaikhrezwan66@gmail.com>
2023-11-20 02:09:44 +0600

    installed react-stripe-js as payment gateway and created CardElement

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,4 +1,10 @@
+import { loadStripe } from '@stripe/stripe-js';
 import SectionTitle from '../../../components/SectionTitle';
+import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from './CheckoutForm';
+
+// TODO: add publishable key (PK) from website
+const stripePromise = loadStripe('');
 
 const Payment = () => {
   return (
@@ -7,7 +13,11 @@ const Payment = () => {
         heading="payment"
         subHeading="Please pay here to order"
       ></SectionTitle>
-      <div className="min-h-screen max-w-4xl mx-auto"></div>
+      <div className="min-h-screen max-w-4xl mx-auto">
+        <Elements stripe={stripePromise}>
+          <CheckoutForm></CheckoutForm>
+        </Elements>
+      </div>
     </div>
   );
 };
