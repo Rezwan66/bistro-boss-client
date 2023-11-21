60ddd9c427ba9e72b8a6981793b9ec45a120a685
Rezwan66 <shaikhrezwan66@gmail.com>
2023-11-22 02:50:37 +0600

    displayed bar chart data of sold categories by quantity

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -2,6 +2,9 @@ import { useQuery } from '@tanstack/react-query';
 import useAuth from '../../../hooks/useAuth';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
 import { FaListAlt, FaTruck, FaUsers, FaWallet } from 'react-icons/fa';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
+
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
 
 const AdminHome = () => {
   const { user } = useAuth();
@@ -15,7 +18,33 @@ const AdminHome = () => {
     },
   });
 
-  console.log(stats);
+  const { data: chartData = [] } = useQuery({
+    queryKey: ['order-stats'],
+    queryFn: async () => {
+      const res = await axiosSecure.get('/order-stats');
+      return res.data;
+    },
+  });
+
+  console.log(stats, chartData);
+
+  //   custom shapes for the bar chart
+  const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${
+      x + width / 2
+    },${y + height / 3}
+    ${x + width / 2}, ${y}
+    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${
+      x + width
+    }, ${y + height}
+    Z`;
+  };
+
+  const TriangleBar = props => {
+    const { fill, x, y, width, height } = props;
+
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+  };
 
   return (
     <div className="p-10">
@@ -23,7 +52,7 @@ const AdminHome = () => {
         <span>Hi, Welcome </span>
         {user?.displayName ? user?.displayName : 'Back'}
       </h2>
-      <div className="my-10">
+      <div className="mt-6">
         <div className="stats rounded-none text-white gap-6">
           <div className="stat rounded-md flex items-center py-8 bg-gradient-to-r from-[#BB34F5] to-[#FCDBFF] px-10">
             <div className="text-white">
@@ -69,6 +98,37 @@ const AdminHome = () => {
               </div>
             </div>
           </div>
+        </div>
+        {/* charts */}
+        <div className="flex mt-10">
+          <div className="w-1/2">
+            <BarChart
+              width={500}
+              height={300}
+              data={chartData}
+              margin={{
+                top: 20,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="category" />
+              <YAxis />
+              <Bar
+                dataKey="quantity"
+                fill="#8884d8"
+                shape={<TriangleBar />}
+                label={{ position: 'top' }}
+              >
+                {chartData.map((entry, index) => (
+                  <Cell key={`cell-${index}`} fill={colors[index % 6]} />
+                ))}
+              </Bar>
+            </BarChart>
+          </div>
+          <div className="w-1/2"></div>
         </div>
       </div>
     </div>
